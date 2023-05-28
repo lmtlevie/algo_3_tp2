668ccab42e318a8e2bc97b72f5776674d136d603
Braian Edgardo Rodriguez Calzada <96453606+Sc4r1fy@users.noreply.github.com>
2023-05-28 03:11:44 -0300

    Este algoritmo deberia andar...
    para cada componente conexa me fijo:
      la cantidad de aristas que hay en un ciclo, a la vez, guardo la cantidad de nodos involucrados en estas aristas.
      calculo la probabilidad de elegir 2 nodos que esten dentro de estos nodos ganadores para esta componente conexa. 
        es decir nodos_ganadores/formas_de_jugar.
    
    luego sumo las probabilidades de ganar en cada componente conexa.
    testear en papel, funciona!
    el tema es que hay algun caso que está fallando. tengo que ver qué caso rompe

---
+++ ej_1_brai.cpp
@@ -0,0 +1,122 @@
+#include <iostream>
+#include <vector>
+#include <set>
+#include <iomanip>
+using namespace std;
+
+//https://cp-algorithms.com/graph/bridge-searching.html
+// el codigo esta mal, la idea esta cerca, con varios casos de test en papel prece funcionar
+// usar array globales
+
+
+void dfs(int v,vector<set<int>> &vecinos,vector<bool> &visitado,int n,int &aristas_en_ciclo,int p,set<pair<int,int>> &back_edges,vector<int> &previo){
+    
+    visitado[v] = true;
+    for(int u: vecinos[v]){
+        if(!visitado[u]){
+            previo[u] = v;
+            //cout << "previo: " << u << "-" << v << endl;
+            dfs(u,vecinos,visitado,n,aristas_en_ciclo,v,back_edges,previo);
+        }else if(u != p){
+            //if (previo[u] == -1) {previo[u]=v;}
+            back_edges.insert(make_pair(u,v));
+        }
+    }
+
+}
+
+
+
+double probabilidad_de_perder(vector<set<int>> &vecinos,int n){
+    
+    vector<bool> visitados(n+1,false);
+    int aristas_en_ciclo = 0; // cuento las aristas que pertenecen a un ciclo talque aunque el adversario que juega perfecto las elimine sea jugada ganadora
+    
+    set<pair<int,int>> back_edges;
+    vector<int> previo(n+1,-1);
+    double probabilidad_de_ganar = 0;
+    long long formas_de_jugar = (n * (n-1))/2;
+    vector<vector<bool>> aristas_procesadas;
+    aristas_procesadas.assign(n+1, vector<bool>(n+1,false));
+    vector<bool> nodos_procesados(n+1,false);
+    for(int i=1;i <= n;i++){
+        if(!visitados[i]){
+          //  cout << "Componente Conexa" << endl;
+            
+            dfs(i,vecinos,visitados,n,aristas_en_ciclo,-1,back_edges,previo);
+
+            int nodos_componente = 0;
+            int aristas_no_puentes = 0;
+            for(auto x: back_edges){
+                if( !(aristas_procesadas[x.first][x.second] ||  aristas_procesadas[x.second][x.first]) ) {
+                  aristas_procesadas[x.first][x.second] = true;
+                  aristas_procesadas[x.second][x.first] = true;
+                  aristas_en_ciclo++;
+                  aristas_no_puentes++;
+                  if( !nodos_procesados[x.first] ){ nodos_procesados[x.first] = true; nodos_componente++;}
+                }
+                
+                
+                //cout << x.first << "-" << x.second << endl;
+                int u = x.first;
+                int v = x.second;
+
+                while(v != u){
+                    if( previo[v]==-1 || aristas_procesadas[v][previo[v]] || aristas_procesadas[previo[v]][v] ) break;
+                    //cout << v << endl;
+                    if(!nodos_procesados[v]){ nodos_componente++;}
+                    aristas_en_ciclo++;
+                    aristas_no_puentes++;
+                    aristas_procesadas[v][previo[v]] = true;
+                    aristas_procesadas[previo[v]][v] = true;
+                    v = previo[v];
+        }
+    }
+    
+            probabilidad_de_ganar += ( ((double)nodos_componente*( (double) nodos_componente-1) ) / 2 ) / (double) formas_de_jugar;
+    
+    
+    
+        }
+        
+    }
+   
+    
+    
+    
+
+    double res = 1 - probabilidad_de_ganar;
+  
+    return res;
+}
+
+
+int main(){
+
+    int n; cin >> n;
+    int m; cin >> m;
+    // uso lista de adyacencia
+    vector<set<int>> vecinos(n+1);
+
+    while(m--){
+        int v; cin >> v;
+        int w; cin >> w;
+        vecinos[v].insert(w);
+        vecinos[w].insert(v);
+    }
+
+    double res = probabilidad_de_perder(vecinos,n);
+    cout << fixed << setprecision(5) << res << endl;
+
+    return 0;
+
+}
+
+
+
+
+
+
+
+
+
